ac0b6d5d53174951e42214772dee1fe5af0bc0e5
Varun Ajith <114599089+Varun-Ajith@users.noreply.github.com>
2025-03-07 13:52:27 +0530

    Update trajectory_visualization.launch.py

--- trajectory_visualization/launch/trajectory_visualization.launch.py
+++ trajectory_visualization/launch/trajectory_visualization.launch.py
@@ -3,6 +3,7 @@ from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
     pkg_share = get_package_share_directory('trajectory_visualization')
     rviz_config_file = os.path.join(pkg_share, 'rviz', 'trajectory_visualization.rviz')
